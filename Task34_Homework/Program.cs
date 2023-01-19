9dc49e8970c72a57e1087ff9bd47bd7803ec8822
Marina Zyazina <zyazinamv@gmail.com>
2023-01-19 22:24:15 +0200

    Added Tasks 31-38

---
+++ Task34_Homework/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// Пример: [345, 897, 568, 234] -> 2
+
+int[] array = CreateArrayRndInt(6, -1000, 1000);
+PrintArray(array);
+
+int countEvenElements = CountEvenElements(array);
+Console.WriteLine($"Количество чётных чисел в массиве] = {countEvenElements}");
+
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ",");
+        else Console.Write(arr[i]);
+    }
+    Console.WriteLine("]");
+}
+
+int CountEvenElements(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0) count ++;
+    }
+    return count;
+}
---
+++ Task36_Homework/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// Примеры: [3, 7, 23, 12] -> 19; [-4, -6, 89, 6] -> 0
+
+int[] array = CreateArrayRndInt(4, -100, 100);
+PrintArray(array);
+
+int sumElementsAtOddPositions = SumElementsAtOddPositions(array);
+Console.WriteLine($"Сумма элементов на нечетных позициях = {sumElementsAtOddPositions}");
+
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ",");
+        else Console.Write(arr[i]);
+    }
+    Console.WriteLine("]");
+}
+
+int SumElementsAtOddPositions(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 != 0) sum += arr[i];
+    }
+    return sum;
+}
